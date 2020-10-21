91a2b4d8be1038033233d86eb0b259bb539afc97
shivam giri <mahant1598sivam@gmail.com>
2020-10-21 07:18:20 -0400

    button modified

--- pages/quiz/start.vue
+++ pages/quiz/start.vue
@@ -7,15 +7,15 @@
           name="welcome_form"
           onsubmit="submitForm(event)"
         >
-          <input class="block container p-4 text-center px-4 py-2 outline-none" type="text" name="name" placeholder="Enter Your Name"/>
+          <input class="block container p-4 text-center px-4 py-2 outline-none rounded-3xl" type="text" name="name" placeholder="Enter Your Name"/>
           <nuxt-link to = "/quiz/quiz">
-          <button class="block container p-5 rounded-3xl text-white text-3xl cursor-pointer transform motion-safe:hover:-translate-y-1 motion-safe:hover:scale-110 transition ease-in-out duration-300">Start Quiz</button>
+          <button class="block container bg-blue-300 p-4 mt-4 rounded-3xl text-white text-3xl cursor-pointer transform motion-safe:hover:-translate-y-1 motion-safe:hover:scale-110 transition ease-in-out duration-300">Start Quiz</button>
           </nuxt-link>
         </form>
       </div>
     </div>
 
- 
+
 </template>
 
 <script>
