d357b44d7b84c450e8caaa191db9c369e1de4252
shivam giri <mahant1598sivam@gmail.com>
2020-10-21 04:14:18 -0400

    tailwing added

--- pages/quiz/start.vue
+++ pages/quiz/start.vue
@@ -1,65 +1,19 @@
 <template>
-  <!-- Navbar -->
-  <div>
-    <nav
-      class="top-0 absolute z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg"
-    >
-      <div class="container px-4 mx-auto flex flex-wrap items-center justify-between">
-        <div
-          class="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start"
+
+  <div class="flex justify-center item-center max-w-full max-h-screen h-full bg-blue-700">
+      <div class="welcome_text">
+        <form
+          class="welcome_form"
+          name="welcome_form"
+          onsubmit="submitForm(event)"
         >
-          <a
-            class="text-lg font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-white"
-            href="https://www.creative-tim.com/learning-lab/tailwind-starter-kit#/presentation"
-          >The Power Room</a>
-          <button
-            class="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
-            type="button"
-            onclick="toggleNavbar('example-collapse-navbar')"
-          >
-            <i class="text-white fas fa-bars"></i>
-          </button>
-        </div>
-        <div
-          class="lg:flex flex-grow items-center bg-white lg:bg-transparent lg:shadow-none hidden"
-          id="example-collapse-navbar"
-        >
-          <ul class="flex flex-col lg:flex-row list-none lg:ml-auto">
-            <!-- Icon 1 -->
-            <li class="flex items-center">
-              <a
-                class="lg:text-white lg:hover:text-gray-300 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-                href="#pablo"
-              >
-                <i class="text-orange-500 fab fa-facebook text-lg leading-lg"></i>
-                <span class="lg:hidden inline-block ml-2">Share</span>
-              </a>
-            </li>
-            <!-- Icon 2 -->
-            <li class="flex items-center">
-              <a
-                class="lg:text-white lg:hover:text-gray-300 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-                href="#pablo"
-              >
-                <i class="text-orange-500 fab fa-twitter text-lg leading-lg"></i>
-                <span class="lg:hidden inline-block ml-2">Tweet</span>
-              </a>
-            </li>
-            <!-- Icon 3 -->
-            <li class="flex items-center">
-              <a
-                class="lg:text-white lg:hover:text-gray-300 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-                href="#pablo"
-              >
-                <i class="text-orange-500 fab fa-linkedin text-lg leading-lg"></i>
-                <span class="lg:hidden inline-block ml-2">Profile</span>
-              </a>
-            </li>
-          </ul>
-        </div>
+          <input class="block container p-4 text-center px-4 py-2 outline-none" type="text" name="name" placeholder="Enter Your Name"/>
+          <button class="block container p-5 rounded-3xl text-white text-3xl cursor-pointer transform motion-safe:hover:-translate-y-1 motion-safe:hover:scale-110 transition ease-in-out duration-300">Start Quiz</button>
+        </form>
       </div>
-    </nav>
-  </div>
+    </div>
+
+ 
 </template>
 
 <script>
